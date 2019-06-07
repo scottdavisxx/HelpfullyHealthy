38521dbcf39e4607a3982619f37de62ab6860d6c
Jose Barajas <barajas.jose452yahoo.com>
2019-06-06 18:02:15 -0700

    resources.js created

---
+++ js/resources.js
@@ -0,0 +1,41 @@
+const resources = [
+    {
+        name:'Neddle Exchange',
+        phone:'5594126226',
+        address:'1101 W Hedges Ave, Fresno CA',
+        tags:['needles']
+    },
+    {
+        name:'Valley Recovery Center of California at Fresno',
+        phone:'8772194297',
+        address:'6435 N Palm Ave, Fresno CA',
+        tags:['Drug-Rehab']
+    },
+    {
+        name:'Fresno County Department of Public Health',
+        phone:'5596006404',
+        address:'1221 Fulton Mall, Fresno CA',
+        tags:['Aids-Testing']       
+    }
+]
+
+const drugs = [
+    {
+        name:'Meth',
+        effects:'Symptons pf Meth use include: sudden or severe weight loss, premature aging of the skin,slow healing sores,blackened rotting teeth,kidney failure,liver failure,hallucination,paranoia,and death.',
+        Description:`The use of Methamphetime(Meth) is dangerous as a highly addictive drug users will develop an increasing need for it to the point it will envelop most of their waking thoughts.It can develop a strong grip on an individuals mental and physical state. The addiction may lead many to engage in riskier behaviors in order to procure more and to neglect their responsabilities for their next rush.`,
+        tags:['Meth','Addiction','blackened rotting teeth','Sores','Paranoia','Hallucination','Methamphetime']
+    },
+    {
+        name:'Valley Recovery Center of California at Fresno',
+        phone:'8772194297',
+        address:'6435 N Palm Ave, Fresno CA',
+        tags:'Drug-Rehab'
+    },
+    {
+        name:'Fresno County Department of Public Health',
+        phone:'5596006404',
+        address:'1221 Fulton Mall, Fresno CA',
+        tags:'Aids-Testing'       
+    }
+]
