15a3e21eff400e03335f22ea0a892e030aff1d42
Jose Barajas <barajas.jose452yahoo.com>
2019-06-13 19:18:40 -0700

    landing page

--- js/resources.js
+++ js/resources.js
@@ -16,6 +16,24 @@ const resources = [
         phone:'5596006404',
         address:'1221 Fulton Mall, Fresno CA',
         tags:['Aids-Testing']       
+    },
+    {
+        name:'Ted C. Wills Community Center',
+        phone:'5596217529',
+        address:'770 N San Pablo Ave, Fresno, CA 93728',
+        tags:['Cooling','Center','Fresno','Hot']
+    },
+    {
+        name:'Frank H. Ball Neighborhood Center',
+        phone:'5594881502',
+        address:' 760 Mayor Ave, Fresno, CA 93706',
+        tags:['Cooling','Center','Fresno','Hot']
+    },
+    {
+        name:'Fresno County Department of Public Health',
+        phone:'5596006404',
+        address:'1221 Fulton Mall, Fresno CA',
+        tags:['Aids-Testing']       
     }
 ]
 
@@ -23,14 +41,14 @@ const drugs = [
     {
         name:'Meth',
         effects:'Symptons pf Meth use include: sudden or severe weight loss, premature aging of the skin,slow healing sores,blackened rotting teeth,kidney failure,liver failure,hallucination,paranoia,and death.',
-        Description:`The use of Methamphetime(Meth) is dangerous as a highly addictive drug users will develop an increasing need for it to the point it will envelop most of their waking thoughts.It can develop a strong grip on an individuals mental and physical state. The addiction may lead many to engage in riskier behaviors in order to procure more and to neglect their responsabilities for their next rush.`,
+        description:`The use of Methamphetime(Meth) is dangerous as a highly addictive drug users will develop an increasing need for it to the point it will envelop most of their waking thoughts.It can develop a strong grip on an individuals mental and physical state. The addiction may lead many to engage in riskier behaviors in order to procure more and to neglect their responsabilities for their next rush.`,
         tags:['Meth','Addiction','blackened rotting teeth','Sores','Paranoia','Hallucination','Methamphetime']
     },
     {
-        name:'Valley Recovery Center of California at Fresno',
-        phone:'8772194297',
-        address:'6435 N Palm Ave, Fresno CA',
-        tags:'Drug-Rehab'
+        name:'Heat Stroke',
+        effects:['Confusion','Coma','Dry Skin','Slurred Speach'],
+        description:'Heat stroke is the most serious heat-related illness. It occurs when the body becomes unable to control its temperature: the body’s temperature rises rapidly, the sweating mechanism fails, and the body is unable to cool down. When heat stroke occurs, the body temperature can rise to 106°F or higher within 10 to 15 minutes. Heat stroke can cause death or permanent disability if emergency treatment is not given.',
+        tags:['Heat','Stroke','Stress','Confusion','Coma','Dry Skin','Slurred Speach']
     },
     {
         name:'Fresno County Department of Public Health',
